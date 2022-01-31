564f3fddbbae3ddbd27b99ca4934f9ba6b41df22
User <User@LAPTOP-C7VL0LDM>
2022-01-31 16:43:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diffrence_Write_and_WriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static void Diffrence()
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+}
